aa036dbab749ee3fd45ddefb4b973ad513d9343a
TreantProtectorGo <wing.ws.lee@gmail.com>
2023-08-23 09:59:46 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: isProduction ? '/vue-bootstrap/' : '/'
+  };
+});
